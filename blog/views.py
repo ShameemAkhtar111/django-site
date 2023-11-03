fb004eec2aff9805201313666f535ad0604924a0
Shameem <shameemakhtar111@gmail.com>
2023-11-03 22:30:59 +0530

    added views placeholder

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+def starting_page(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def posts_details(request):
+    pass
