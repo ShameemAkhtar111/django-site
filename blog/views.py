3123dfad0f48211069b9e44ede877e621b662809
Shameem <shameemakhtar111@gmail.com>
2023-11-04 22:34:27 +0530

    added views placeholder

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def starting_page(request):
-    pass
+    return render(request, "blog/index.html")
 
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
 
 def posts_details(request):
